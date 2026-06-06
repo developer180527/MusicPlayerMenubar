47614f2ec3d3fea40dde7f166b4107fa3bd8b133
developer180527 <venugopal9836n@gmail.com>
2026-06-06 10:40:40 +0530

    Initial Commit

---
+++ MusicPlayerMenubar/MusicPlayerMenubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerMenubarApp.swift
+//  MusicPlayerMenubar
+//
+//  Created by Venu Gopal on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerMenubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
